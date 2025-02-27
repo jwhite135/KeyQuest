d361561a5b24b78fc2f297851a6f233a991e4db6
Matthew Radin <mgradin@email.sc.edu>
2025-02-27 15:09:29 -0500

    completed issue #3

---
+++ src/main/java/com/model/DataWriter.java
@@ -0,0 +1,23 @@
+package com.model;
+
+public class DataWriter {
+    public static void saveSongs() {
+        return;
+    }
+
+    public static void saveUsers() {
+        return;
+    }
+
+    public static void savePosts() {
+        return;
+    }
+
+    public static void saveComments() {
+        return;
+    }
+
+    public static void saveLessons() {
+        return;
+    }
+}
