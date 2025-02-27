6376a50dd397b5f263cc6257d60c20a24f586457
Ian Attmore <ian.attmore@outlook.com>
2025-02-26 19:39:21 -0500

    setting up initial project

---
+++ keyquestjavafx/src/main/java/com/model/keyquest.java
@@ -0,0 +1,7 @@
+package com.model;
+
+public class keyquest {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
--- keyquestjavafx/src/main/java/module-info.java
+++ keyquestjavafx/src/main/java/module-info.java
@@ -1,7 +1,12 @@
 module com.keyquestjavafx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires json.simple;
+
 
     opens com.keyquestjavafx to javafx.fxml;
     exports com.keyquestjavafx;
+    
+    opens com.model to javafx.fxml;
+    exports com.model;
 }
