f8b05ae0982c5a93b2fd4dd50d9a588ca04be519
Matthew Radin <mgradin@email.sc.edu>
2025-02-27 15:21:48 -0500

    completed issue #3

---
+++ src/main/java/com/model/DataLoader.java
@@ -0,0 +1,24 @@
+package com.model;
+import java.util.ArrayList;
+
+public class DataLoader {
+    public ArrayList<Song> getSongs() {
+        return new ArrayList<Song>();
+    }
+
+    public ArrayList<User> getUsers() {
+        return new ArrayList<User>();
+    }
+
+    public ArrayList<Post> getPosts() {
+        return new ArrayList<Post>();
+    }
+
+    public ArrayList<Comment> getComments() {
+        return new ArrayList<Comment>();
+    }
+
+    public ArrayList<Lesson> getLessons() {
+        return new ArrayList<Lesson>();
+    }
+}
