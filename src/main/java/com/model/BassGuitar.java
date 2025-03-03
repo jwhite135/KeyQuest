875e34acfbe7e26c8bcc508a3e3f2fa6bb0ef56c
Ryan Leadbitter <rdleadbitter@gmail.com>
2025-03-03 17:37:39 -0500

    closed #51; created BassGuitar stub

---
+++ src/main/java/com/model/BassGuitar.java
@@ -0,0 +1,23 @@
+package com.model;
+
+import java.util.ArrayList;
+
+public class BaseGuitar {
+    private ArrayList<Note> notes;
+    
+    public BassGuitar() {
+
+    }
+
+    public void displayBassGuitar() {
+
+    }
+
+    public void playNote(Note note) {
+
+    }
+
+    public void playSong(Song song) {
+        
+    }
+}
