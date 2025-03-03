c823fb4ad415aea72c85c30e78bc17790f1747ac
Ryan Leadbitter <rdleadbitter@gmail.com>
2025-03-03 16:50:51 -0500

    closed #46

---
+++ src/main/java/com/model/Instrument.java
@@ -0,0 +1,20 @@
+package src.main.java.com.model;
+
+public class Instrument {
+    private String name;
+    private String description;
+    private Note minNote;
+    private Note maxNote;
+
+    public Instrument() {
+
+    }
+
+    public void playNote(Note note) {
+
+    }
+
+    public void playSong(Song song) {
+        
+    }
+}
