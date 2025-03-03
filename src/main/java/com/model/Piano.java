ef0b90a6709b09dadb7434a503f7d4c30a3941f3
Ryan Leadbitter <rdleadbitter@gmail.com>
2025-03-03 17:15:28 -0500

    closed #47

---
+++ src/main/java/com/model/Piano.java
@@ -0,0 +1,25 @@
+package src.main.java.com.model;
+
+import java.util.ArrayList;
+
+public class Piano {
+    private ArrayList<PianoNote> notes;
+    private PianoNote minNote;
+    private PianoNote maxNote;
+
+    public Piano() {
+
+    }
+
+    public void displayPiano() {
+
+    }
+
+    public void playNote(Note note) {
+
+    }
+
+    public void playSong(Song song) {
+        
+    }
+}
