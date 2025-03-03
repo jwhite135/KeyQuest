05ce32388aa07b31b5fe0df8cf9fc65bb5a2ea3d
Ryan Leadbitter <rdleadbitter@gmail.com>
2025-03-03 18:15:24 -0500

    closed #35; created PianoMeasure stub

---
+++ src/main/java/com/model/PianoMeasure.java
@@ -0,0 +1,16 @@
+package src.main.java.com.model;
+
+import java.util.ArrayList;
+
+public class PianoMeasure extends Measure {
+    private boolean showClef;
+
+    public PianoMeasure(boolean showClef, ArrayList<Chord> chords) {
+        super(chords);
+        this.showClef = showClef;
+    }
+    
+    public void playMeasure() {
+
+    }
+}
