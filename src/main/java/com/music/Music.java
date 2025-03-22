b349eb583d462fb75c22085f8b21633a6b22ae4b
Ryan Leadbitter <rdleadbitter@gmail.com>
2025-03-22 15:36:33 -0400

    wrote test song for multiple voices

--- src/main/java/com/music/Music.java
+++ src/main/java/com/music/Music.java
@@ -7,7 +7,8 @@ public class Music {
     public static void main(String[] args) {
         Player player = new Player();
 
-        String music = 
+        String music = "T84 ";
+        music +=
             "V0 E3Bi Db3i Ab4q Ab4q C4i Bb4i Ab4q Ab4q Eb3q F3q Ab4q F3q Eb3h | " + 
             "V1 E3Bi D3bi C3q Eb3q Ab4i G4i Eb3q Eb3q Eb3q Db3q F3q Db3q Db3q- C3q | " + 
             "V2 G2q Ab3q Ab3q C3i Bb3i C3q C3q Ab3q Ab3q Ab3q Ab3q Ab3h | " + 
