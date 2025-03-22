86b91a03ca7a0c74b48c82d49fccfa116f3ea828
MatthewRadin <143623355+MatthewRadin@users.noreply.github.com>
2025-03-22 16:18:44 -0400

    Fixed small errors that resulted from changing note to abstract

--- src/main/java/com/model/Rest.java
+++ src/main/java/com/model/Rest.java
@@ -2,7 +2,15 @@ package com.model;
 
 public class Rest extends Note{
     
-    public Rest(int length) {
+    public Rest(String length) {
         super(length);
+    }
+
+    /**
+     * Rest does not make sound, so it doesn't play.
+     */
+    @Override
+    public void playNote() {
+        return;
     }
 }
