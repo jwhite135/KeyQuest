457bfef211b7e4a4462558bc20737ed84f3ceb6c
Ryan Leadbitter <rdleadbitter@gmail.com>
2025-03-25 12:10:49 -0400

    added JavaDoc comments to Chord

--- src/main/java/com/model/Chord.java
+++ src/main/java/com/model/Chord.java
@@ -4,14 +4,27 @@ import java.util.ArrayList;
 
 import org.jfugue.player.Player;
 
+/**
+ * Chord class that holds a list of notes
+ * @author Ryan Leadbitter
+ */
 public class Chord {
     private ArrayList<Note> notes;
     private static Player player = new Player();
     
+    /**
+     * Constructor for Chord
+     * @param notes list of notes
+     */
     public Chord(ArrayList<Note> notes) {
         this.notes = notes;
     }
 
+    /**
+     * Plays the chord using jFugue player
+     * Loops through notes and plays them
+     * Adds each note to a string that is used an as argument by the player
+     */
     public void playChord() {
         String playThis = "";
         for (int i = 0; i < notes.size(); ++i) {
