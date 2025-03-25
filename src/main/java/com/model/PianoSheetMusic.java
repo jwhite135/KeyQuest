8c35eaed1c485c168a4bc1048696d239b35a1ec0
Ian Attmore <ian.attmore@outlook.com>
2025-03-25 12:06:13 -0400

    adding printSheet music method

--- src/main/java/com/model/PianoSheetMusic.java
+++ src/main/java/com/model/PianoSheetMusic.java
@@ -8,4 +8,15 @@ public class PianoSheetMusic extends SheetMusic {
         this.key = key;
         this.major = major;
     }
+
+    public void printSheetMusic() {
+        System.out.println("Key: " + key);
+        System.out.println("Major: " + major);
+        System.out.println("Tempo: " + getTempo());
+        System.out.println("Time Signature: " + getTimeSigNum() + "/" + getTimeSigDen());
+        System.out.println("Measures: ");
+        for (Measure measure : getMeasures()) {
+            measure.printMeasure();
+        }
+    }
 }
