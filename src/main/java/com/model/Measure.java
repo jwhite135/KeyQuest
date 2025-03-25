efb09f9f3ed133c7e2a493b6d4b09b523474dbd5
Ryan Leadbitter <rdleadbitter@gmail.com>
2025-03-25 11:52:47 -0400

    added JavaDoc for Measure.java

--- src/main/java/com/model/Measure.java
+++ src/main/java/com/model/Measure.java
@@ -2,19 +2,35 @@ package com.model;
 
 import java.util.ArrayList;
 
+/**
+ * Measure class that holds a list of chords
+ * @author Ryan Leadbitter
+ */
 abstract class Measure {
     protected ArrayList<Chord> chords;
 
+    /**
+     * Constructor for Measure
+     * @param chords list of chords
+     */
     public Measure(ArrayList<Chord> chords) {
         this.chords = chords;
     }
 
+    /**
+     * Method to play the measure
+     * Loops through the chords and plays them
+     */
     public void playMeasure() {
         for (int i = 0; i < chords.size(); i++) {
             chords.get(i).playChord();
         }
     }
 
+    /**
+     * Method to get the chords list
+     * @return ArrayList<Chord>
+     */
     public ArrayList<Chord> getChords() {
         return chords;
     }
