35026da1d0e1b8136cf0ae41f7eccfbee14031ef
Ryan Leadbitter <rdleadbitter@gmail.com>
2025-03-25 12:07:01 -0400

    added JavaDoc comments for PianoNote

--- src/main/java/com/model/PianoNote.java
+++ src/main/java/com/model/PianoNote.java
@@ -2,6 +2,10 @@ package com.model;
 
 import org.jfugue.player.Player;
 
+/**
+ * PianoNote class that extends Note and implements playNote method
+ * @author Ryan Leadbitter
+ */
 public class PianoNote extends Note {
     private String key;
     private boolean sharp;
@@ -23,6 +27,13 @@ public class PianoNote extends Note {
         Examples of combined notation: C4q, F#5i, Ab3w
     */
 
+    /**
+     * Constructor for PianoNote
+     * @param length length of note (w, h, q, i, s)
+     * @param key Piano key/name of note (A-G)
+     * @param sharp true if sharp (#)
+     * @param flat true if flat (b)
+     */
     public PianoNote(String length, String key, boolean sharp, boolean flat) {
         super(length);
         this.key = key;
@@ -30,11 +41,18 @@ public class PianoNote extends Note {
         this.flat = flat;
     }
 
+    /**
+     * Plays the note using jFugue player
+     */
     @Override
     public void playNote() {
         player.play(getNote());
     }
     
+    /**
+     * Returns the note in jFugue notation
+     * @return String
+     */
     public String getNote() {
         if (sharp) {
             return key + "#" + length;
@@ -45,18 +63,35 @@ public class PianoNote extends Note {
         }
     }
 
+    /**
+     * Returns the key of the note
+     * @return String
+     */
     public String getKey() {
         return key;
     }
 
+    /**
+     * Returns if the note is sharp
+     * @return boolean
+     */
     public boolean isSharp() {
         return sharp;
     }
 
+    /**
+     * Returns if the note is flat
+     * @return boolean
+     */
     public boolean isFlat() {
         return flat;
     }
 
+    /**
+     * Returns the length of the note
+     * @return String
+     */
+    @Override
     public String getLength() {
         return length;
     }
