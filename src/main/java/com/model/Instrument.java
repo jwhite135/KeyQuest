7209730e0c26fa34e96aa7f1471012e44ced4b8f
Ryan Leadbitter <rdleadbitter@gmail.com>
2025-03-25 11:57:22 -0400

    added JavaDoc for Measure.java

--- src/main/java/com/model/Instrument.java
+++ src/main/java/com/model/Instrument.java
@@ -1,29 +1,16 @@
 package com.model;
 
-/**
- * Instrument class that represents a musical instrument.
- * @author Ryan Leadbitter
- */
 public class Instrument {
     private String name;
     private Note minNote;
     private Note maxNote;
 
-    /**
-     * Default onstructor for Instrument class.
-     * Sets the name of the instrument to "Piano".
-     * Sets the minNote and maxNote to null.
-     */
     public Instrument() {
         this.name = "Piano";
         this.minNote = null;
         this.maxNote = null;
     }
 
-    /**
-     * 
-     * @param note
-     */
     public void playNote(Note note) {
         return;
     }
