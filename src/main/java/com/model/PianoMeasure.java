59eeda3d88a3f25e208b76bce04a557e1561f84a
Ryan Leadbitter <rdleadbitter@gmail.com>
2025-03-25 12:00:51 -0400

    added JavaDoc for PianoMeasure

--- src/main/java/com/model/PianoMeasure.java
+++ src/main/java/com/model/PianoMeasure.java
@@ -2,14 +2,28 @@ package com.model;
 
 import java.util.ArrayList;
 
+/**
+ * PianoMeasure class that extends Measure
+ * @author Ryan Leadbitter
+ */
 public class PianoMeasure extends Measure {
     private boolean showClef;
 
+    /**
+     * Constructor for PianoMeasure
+     * @param showClef boolean to show clef
+     * @param chords list of chords
+     * Calls Measure super constructor
+     */
     public PianoMeasure(boolean showClef, ArrayList<Chord> chords) {
         super(chords);
         this.showClef = showClef;
     }
     
+    /**
+     * Method to play the measure
+     * Loops through the chords and plays them using playChord()
+     */
     @Override
     public void playMeasure() {
         for (int i = 0; i < chords.size(); ++i) {
@@ -17,10 +31,18 @@ public class PianoMeasure extends Measure {
         }
     }
 
+    /**
+     * Method to get the showClef boolean
+     * @return boolean
+     */
     public boolean getShowClef() {
         return showClef;
     }
 
+    /**
+     * Method to set the showClef boolean
+     * @param showClef boolean value to set or unset
+     */
     public void setShowClef(boolean showClef) {
         this.showClef = showClef;
     }
