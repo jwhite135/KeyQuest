656407b3848f5ddc74f91a7fe1ca9280d934ce5f
IanAtt <ian.attmore@outlook.com>
2025-04-01 12:37:16 -0400

    working on testing for user

--- src/test/java/com/model/UserTest.java
+++ src/test/java/com/model/UserTest.java
@@ -1,7 +1,9 @@
 package com.model;
 
-import static org.junit.Assert.*;
+import java.util.UUID;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 
@@ -10,5 +12,13 @@ public class UserTest {
     @Test
     public void testTesting() {
         assertTrue(true);
+    }
+    
+    public void testCreateUser1()
+    {
+        User user = User.getInstance("username", "password", "email");
+        assertEquals("username", user.getUsername());
+        assertEquals("password", user.getPassword());
+        assertEquals("email", user.getEmail());
     }
 }
