ab6acfe426681dc81890f9b0c6b78b17861d6913
Josiah White <jwhite010305@gmail.com>
2025-04-02 01:17:47 -0400

    added tests for DataLoader and DataWriter

--- src/test/java/com/model/DataLoaderTest.java
+++ src/test/java/com/model/DataLoaderTest.java
@@ -1,14 +1,61 @@
 package com.model;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
 
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 
 public class DataLoaderTest {
     
     @Test
-    public void testTesting() {
-        assertTrue(true);
+    public void testLoadingSongs() {
+        SongDatabase songDatabase = SongDatabase.getInstance();
+        assertNotNull(songDatabase.getSongs());
     }
+
+    @Test
+    public void testLoadingUsers() {
+        UserDatabase userDatabase = UserDatabase.getInstance();
+        assertNotNull(userDatabase.getUsers());
+    }
+
+    @Test
+    public void testLoadingPosts() {
+        PostDatabase postDatabase = PostDatabase.getInstance();
+        assertNotNull(postDatabase.getPosts());
+    }
+
+    @Test
+    public void testLoadingSongContents() {
+        SongDatabase songDatabase = SongDatabase.getInstance();
+        ArrayList<Song> songs = songDatabase.getSongs();
+        assertTrue(songs.get(0).getName().equals("Oriental Riff"));
+    }
+
+    @Test
+    public void testLoadingUserContents() {
+        UserDatabase userDatabase = UserDatabase.getInstance();
+        ArrayList<User> users = userDatabase.getUsers();
+        assertTrue(users.get(0).getUsername().equals("josiahGrey"));
+    }
+
+    @Test
+    public void testLoadingPostContents() {
+        PostDatabase postDatabase = PostDatabase.getInstance();
+        ArrayList<Post> posts = postDatabase.getPosts();
+        assertTrue(posts.get(0).getTitle().equals("Bohemian Rhapsody"));
+    }
+
+    @Test
+    public void testLoadingPostComments() {
+        PostDatabase postDatabase = PostDatabase.getInstance();
+        ArrayList<Post> posts = postDatabase.getPosts();
+        assertTrue(posts.get(0).getComments().get(0).getBody().equals("HOLY SHNIKES THIS APP is ok i guess..."));
+    }
+
+    // Test for user lessons in future but not quite implemented yet
+
+    // Could test for null/empty json but hard to do in this case - also should never happen
 }
--- src/test/java/com/model/DataWriterTest.java
+++ src/test/java/com/model/DataWriterTest.java
@@ -1,14 +1,53 @@
 package com.model;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
 
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 
 public class DataWriterTest {
     
     @Test
-    public void testTesting() {
-        assertTrue(true);
+    public void testWritingSongs() {
+        SongDatabase songDatabase = SongDatabase.getInstance();
+        ArrayList<Song> ogSongs = songDatabase.getSongs();
+        songDatabase.save();
+        DataLoader.getSongs();
+        ArrayList<Song> newSongs = songDatabase.getSongs();
+        assertTrue(ogSongs.containsAll(newSongs) && newSongs.containsAll(ogSongs));
     }
+
+    @Test
+    public void testWritingUsers() {
+        UserDatabase userDatabase = UserDatabase.getInstance();
+        ArrayList<User> ogUsers = userDatabase.getUsers();
+        userDatabase.save();
+        DataLoader.getUsers();
+        ArrayList<User> newUsers = userDatabase.getUsers();
+        assertTrue(ogUsers.containsAll(newUsers) && newUsers.containsAll(ogUsers));
+    }
+
+    @Test
+    public void testWritingPosts() {
+        PostDatabase postDatabase = PostDatabase.getInstance();
+        ArrayList<Post> ogPosts = postDatabase.getPosts();
+        postDatabase.save();
+        DataLoader.getPosts();
+        ArrayList<Post> newPosts = postDatabase.getPosts();
+        assertTrue(ogPosts.containsAll(newPosts) && newPosts.containsAll(ogPosts));
+    }
+
+    @Test
+    public void testWritingPostComments() {
+        PostDatabase postDatabase = PostDatabase.getInstance();
+        ArrayList<Comment> ogComments = postDatabase.getPosts().get(0).getComments();
+        postDatabase.save();
+        DataLoader.getPosts();
+        ArrayList<Comment> newComments = postDatabase.getPosts().get(0).getComments();
+        assertTrue(ogComments.containsAll(newComments) && newComments.containsAll(ogComments));
+    }
+
+    // Test for student lessons in future but not quite implemented yet
+    // Same with user friends, favorites, etc.
 }
