cd8270cfe2ec4d76cdf40ebd9d47e072598d5433
Ryan Leadbitter <rdleadbitter@gmail.com>
2025-04-02 21:19:07 -0400

    added testPlayingPianoMeasure to PianoMeasureTest

--- src/test/java/com/model/PianoMeasureTest.java
+++ src/test/java/com/model/PianoMeasureTest.java
@@ -1,14 +1,38 @@
 package com.model;
 
-import static org.junit.Assert.*;
+import java.util.ArrayList;
 
+import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
 
 
 public class PianoMeasureTest {
     
     @Test
-    public void testTesting() {
-        assertTrue(true);
+    public void testPlayingPianoMeasure() {
+        ArrayList<Chord> chords = new ArrayList<>();
+        ArrayList<Note> cMajor = new ArrayList<>();
+        cMajor.add(new PianoNote("q", "C3", false, false));
+        cMajor.add(new PianoNote("q", "E3", false, false));
+        cMajor.add(new PianoNote("q", "G3", false, false));
+        chords.add(new Chord(cMajor));
+        ArrayList<Note> gMajor = new ArrayList<>();
+        gMajor.add(new PianoNote("q", "G3", false, false));
+        gMajor.add(new PianoNote("q", "B3", false, false));
+        gMajor.add(new PianoNote("q", "D4", false, false));
+        chords.add(new Chord(gMajor));
+        ArrayList<Note> aMinor = new ArrayList<>();
+        aMinor.add(new PianoNote("q", "A3", false, false));
+        aMinor.add(new PianoNote("q", "C4", false, false));
+        aMinor.add(new PianoNote("q", "F4", false, false));
+        chords.add(new Chord(aMinor));
+        ArrayList<Note> fMajor = new ArrayList<>();
+        fMajor.add(new PianoNote("q", "F4", false, false));
+        fMajor.add(new PianoNote("q", "A4", false, false));
+        fMajor.add(new PianoNote("q", "C5", false, false));
+        chords.add(new Chord(fMajor));
+        PianoMeasure measure = new PianoMeasure(false, chords);
+        String output = measure.playMeasure();
+        assertNotNull(output);
     }
 }
