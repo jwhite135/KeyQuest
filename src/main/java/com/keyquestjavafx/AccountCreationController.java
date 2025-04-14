643a7f9f49b79fd5ba6824dc29965b15a83d4dc8
Josiah White <jwhite010305@gmail.com>
2025-04-14 13:59:12 -0400

    fixed #105; added landing page to UI and started on login and account creation pages

--- src/main/java/com/keyquestjavafx/AccountCreationController.java
+++ src/main/java/com/keyquestjavafx/AccountCreationController.java
@@ -1,9 +1,10 @@
 package com.keyquestjavafx;
 
 import java.io.IOException;
+
 import javafx.fxml.FXML;
 
-public class SecondaryController {
+public class AccountCreationController {
 
     @FXML
     private void switchToPrimary() throws IOException {
---
+++ src/main/java/com/keyquestjavafx/LandingPageController.java
@@ -0,0 +1,23 @@
+package com.keyquestjavafx;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class LandingPageController {
+
+    @FXML
+    private Button createAccountButton;
+    @FXML
+    private Button logInButton;
+
+    @FXML
+    private void createAccount() throws IOException {
+        App.setRoot("AccountCreation");
+    }
+    @FXML
+    private void logIn() throws IOException {
+        App.setRoot("logIn");
+    }
+}
