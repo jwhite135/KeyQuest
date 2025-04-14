35c4189289e9d91da08f562f5742f27bbb5f2a01
Josiah White <jwhite010305@gmail.com>
2025-04-14 17:15:19 -0400

    fixed #106; made account creation page for UI

--- src/main/java/com/keyquestjavafx/AccountCreationController.java
+++ src/main/java/com/keyquestjavafx/AccountCreationController.java
@@ -2,12 +2,50 @@ package com.keyquestjavafx;
 
 import java.io.IOException;
 
+import com.model.KeyQuestFACADE;
+
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class AccountCreationController {
 
+        @FXML
+    private Button accountCreationButton;
+
     @FXML
-    private void switchToPrimary() throws IOException {
-        App.setRoot("primary");
+    private TextField accountCreationEmailField;
+
+    @FXML
+    private PasswordField accountCreationPasswordField;
+
+    @FXML
+    private TextField accountCreationUsernameField;
+
+    @FXML
+    private ImageView backAccountCreationArrow;
+
+    @FXML
+    private Label errorMessage;
+
+    @FXML
+    void goBack(MouseEvent event) throws IOException {
+        App.setRoot("LandingPage");
     }
+
+    @FXML
+    void createAccount(MouseEvent event) throws IOException {
+        KeyQuestFACADE facade = KeyQuestFACADE.getInstance();
+        if(!facade.makeUser(accountCreationUsernameField.getText(), accountCreationPasswordField.getText(), accountCreationEmailField.getText())) {
+            errorMessage.setText("Invalid inputs or account already exists. Please try again.");
+        }
+        else {
+            App.setRoot("HomePage");
+        }
+    }
+
 }
